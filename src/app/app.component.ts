93d0e7baedb8186be554509353761a2cc86aaf93
Devanshu <anshusingh34718@gmail.com>
2025-03-24 10:58:52 +0530

    modified:   angular.json modified:   src/app/add-info/add-info.component.html modified:   src/app/app.component.ts modified:   src/app/app.config.ts modified:   src/app/app.routes.ts modified:   src/app/document/document.component.html modified:   src/app/footer/footer.component.html modified:   src/app/form/form.component.html modified:   src/app/home/home.component.html modified:   src/app/home/home.component.ts modified:   src/index.html

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,19 +1,37 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, OnInit } from '@angular/core';
+import { NavigationEnd, Router, RouterOutlet } from '@angular/router';
 import { HomeComponent } from "./home/home.component";
 import { FormComponent } from "./form/form.component";
 import { InfoComponent } from "./info/info.component";
 import { DocumentComponent } from "./document/document.component";
 import { AddInfoComponent } from "./add-info/add-info.component";
 import { FooterComponent } from "./footer/footer.component";
+import { filter } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HomeComponent, FormComponent, InfoComponent, DocumentComponent, AddInfoComponent, FooterComponent],
+  imports: [ HomeComponent, FormComponent, InfoComponent, DocumentComponent, AddInfoComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'kusumSolar';
+  constructor(private router: Router) {}
+  ngOnInit() {
+    this.router.events
+      .pipe(filter((event) => event instanceof NavigationEnd))
+      .subscribe((event: NavigationEnd) => {
+        const fragment = this.router.parseUrl(event.url).fragment; // Correct way to get fragment
+        if (fragment) {
+          setTimeout(() => {
+            const element = document.getElementById(fragment);
+            if (element) {
+              element.scrollIntoView({ behavior: 'smooth', block: 'start' });
+            }
+          }, 0);
+        }
+      });
+  }
+
 }
